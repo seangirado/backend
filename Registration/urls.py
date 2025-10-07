c1a2acc8a601d6f3e394858321c988ce1139ef08
seangirado <seangirado@gmail.com>
2025-10-07 12:38:21 +0800

    Updated Views and url

--- Registration/urls.py
+++ Registration/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/register/', views.register_user, name='register_user')
+    path('api/register/', views.register_user, name='register_user'),
+    path('api/users/', views.list_user, name='list_user'),
 ]
